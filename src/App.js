00cb60faf09384eec518d9e9f8f64211b4bdc66f
Rubin Barclay <rubin.barclay@se.experis.com>
2023-01-20 09:18:50 +0100

    added navbar

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import Login from './pages/Login/Login';
 import Translate from './pages/Translate/Translate';
 import Profile from './pages/Profile/Profile';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
+        <Navbar />
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/translate" element={<Translate />} />
