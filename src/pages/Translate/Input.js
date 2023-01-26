77487da853a2dbb2950a13b942fe5fd8e56ba033
Jakob-mbi <jakob_mbi@msn.com>
2023-01-26 08:15:02 +0100

    feat: Input validation

--- src/pages/Translate/Input.js
+++ src/pages/Translate/Input.js
@@ -1,19 +1,28 @@
+import { useForm } from "react-hook-form";
 
-const Input = () => {
+
+const Input = (props) => {
     
+    //const handleInputSubmit = event => { event.preventDefault()}
+    const {register,handleSubmit, formState: {errors}} = useForm();
+    const onSubmit = data => {props.inputClick(data);}
+
     return ( 
         <div className="flex justify-center bg-yellow w-full py-9 not-italic text-lg ">
             <div className="w-4/6 relative">
-                <input className=" px-16 py-4 rounded-full text-2xl w-full" />
-                <img src="https://iconarchive.com/download/i87838/icons8/ios7/Computer-Hardware-Keyboard.ico" className="w-9 h-9 absolute top-3 left-4"/>
-                <a href="#" type="submit" className="bg-purple text-white rounded-full p-1 absolute top-2 right-2">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-10 h-10">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
-                    </svg>
-                </a>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <input {...register("value",{required: 'This is required',pattern:/^[a-z/\s/ ]+$/i})} className=" px-16 py-4 rounded-full text-2xl w-full lowercase" type="text"/>
+                    <img src="https://iconarchive.com/download/i87838/icons8/ios7/Computer-Hardware-Keyboard.ico" alt="Keyboard icon" className="w-9 h-9 absolute top-3 left-4"/>
+                    <button type="submit" className="bg-purple text-white rounded-full p-1 absolute top-2 right-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-10 h-10">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
+                        </svg>
+                    </button>
+                    {errors.value && <p className="text-red-500">Only letters a-z with or with out whitespace is allowd!</p>}
+                </form> 
             </div>    
         </div>
     );
 }
- 
+
 export default Input;
